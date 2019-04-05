ccb4c6b49a27ffcd1f5d764458a3b0c43e9dfe53
Alessandro Ballarin <alessandro.ballarin@finantix.com>
2019-04-05 04:54:45 +0200

    testcase 2.2.14

--- guava/src/com/google/common/hash/HashCode.java
+++ guava/src/com/google/common/hash/HashCode.java
@@ -83,7 +83,7 @@ public abstract class HashCode {
    * @throws IndexOutOfBoundsException if there is not enough room in {@code dest}
    */
   @CanIgnoreReturnValue
-  public int writeBytesTo(byte[] dest, int offset, int maxLength) {
+  public final int writeBytesTo(byte[] dest, int offset, int maxLength) {
     maxLength = Ints.min(maxLength, bits() / 8);
     Preconditions.checkPositionIndexes(offset, offset + maxLength, dest.length);
     writeBytesToImpl(dest, offset, maxLength);
